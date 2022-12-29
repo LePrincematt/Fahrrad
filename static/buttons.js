18f2af197f81dba6f8a98014bb895b86bfa576be
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2022-12-29 10:15:50 +0100

    new code

--- static/buttons.js
+++ static/buttons.js
@@ -8,7 +8,7 @@ function malebikeAvailable() {
     let male_availability = {
         data: document.getElementById('date_malebike')
     }
-    console.log(male_availability);
+    console.log(male_availability.data);
     //console.log("Hat geklappt");
     axios.post('/api/v1/malebike', male_availability)
     .then(function (res) {
