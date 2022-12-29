cc53afcb0a049512bbcc58e3ff3941c94cb9560c
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2022-12-29 10:29:14 +0100

    new

--- static/buttons.js
+++ static/buttons.js
@@ -14,7 +14,7 @@ function malebikeAvailable() {
     .then(function (res) {
         console.log(res);
         let html = "<div>";
-        if(res == "1") {
+        if(res = "1") {
             html += "<p>";
             html += "Verfügbar";
             html += "</p>";
