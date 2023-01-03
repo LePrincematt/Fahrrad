f5ee04d3de0a5d56e0cfa9fdd394ef2125495407
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2023-01-03 19:33:48 +0100

    new

--- static/buttons.js
+++ static/buttons.js
@@ -60,7 +60,7 @@ function checkNumber() {
     console.log("Numbers");
     let html = "<div>";
     html += "<p>";
-    html += "Moin";
+    html += "10";
     html += "</p>";
     html += "</div>";
     document.getElementById("return_availability").innerHTML = html;
